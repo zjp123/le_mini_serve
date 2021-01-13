2023770e569a63101cfca3b38f74c2c3614aa543
zhangjunpeng <zhangjunpeng@lexue.com>
2021-01-13 19:51:12 +0800

    add mongodb.conf

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   jwt_secret: 'lalala', // jwt 密钥
   APPID: 'wx53293bc7eae8f28d',
   APP_SECRET: 'eb0cec75494510a0e6f38de2817f45b6', // 小程序密钥
-  mongodb: 'mongodb://localhost:27017/lexue_mon',
+  mongodb: 'mongodb://0.0.0.0:27017/lexue_mon',
   mongoConfig: {
     autoIndex: false,
     keepAlive: true,
